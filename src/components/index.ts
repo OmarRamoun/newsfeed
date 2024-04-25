63141f568f74ea0676218c28f636203d825086ee
Omar Ramoun <contact@omarramoun.com>
2024-04-25 11:17:36 +0200

    feat(components): add index file

---
+++ src/components/index.ts
@@ -0,0 +1,30 @@
+export * from './typography';
+export * from './box';
+export * from './flex';
+export * from './router';
+export * from './line';
+export * from './touchable-opacity';
+export * from './touchable-without-feedback';
+export * from './pressable';
+export * from './image';
+export * from './empty-state';
+export * from './status';
+export * from './status-bar';
+export * from './safe-area';
+
+export * from './icon';
+
+export * from './lists/flat-list';
+
+export * from './spinner/spinner';
+export * from './spinner/spinner-overlay';
+
+export * from './table/table-body';
+export * from './table/table';
+
+export * from './forms/form';
+export * from './forms/input-formik';
+export * from './forms/input-field/input-text';
+export * from './forms/input-field/input-container';
+export * from './forms/input-field/input-field';
+export * from './forms/label';
