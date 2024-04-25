ccf367c9757b220fc1462b4d08d97e5a9972d687
Omar Ramoun <contact@omarramoun.com>
2024-04-25 11:26:05 +0200

    feat(assets): add all assets

---
+++ src/assets/index.ts
@@ -0,0 +1,19 @@
+/* eslint-disable import/order */
+// SVGs
+import add from './svg/add.svg';
+import check from './svg/check.svg';
+import circle from './svg/circle.svg';
+import cross from './svg/cross.svg';
+import plus from './svg/plus.svg';
+import search from './svg/search.svg';
+import hourglass from './svg/hourglass.svg';
+
+export const iconMap = {
+  add,
+  check,
+  circle,
+  cross,
+  hourglass,
+  plus,
+  search,
+};
